79d7a8f1aacff32786eb09cccc49cf1a677f80b9
BuildTools <unconfigured@null.spigotmc.org>
2023-04-14 22:30:08 -0400

    Add a new class tutorial

---
+++ ConsoleApp1/VariablesAndTypes/Chars.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.VariablesAndTypes
+{
+    internal class Chars : ITestClass
+    {
+
+        //Introduction to C# char type
+        /*
+         C# uses the char type to represent characters. A variable of type char can hold
+        a single character.
+
+        C# provides three ways to represent a character literal:
+            - A character literal
+            - A Unicode escape sequence
+            - A hexadecimal espace sequence
+
+        All three character literals are surrounded by single-quotes
+         */
+
+        char charLiteral = 'a';//This is a character literal
+        char charUnicode = '\u002B'; //A Unicode character starts from \u followed by four-symbol hexadecimal representation of the character
+        char charHexadecimal = '\x002C'; //A hexadecimal character starts from \x and the representation of the character code
+
+        public void Test()
+        {
+            //A character support comparison, equality, increment and decrement
+            char c1 = 'a',
+                c2 = 'a';
+
+            bool result = c1 == c2;
+
+            Console.WriteLine($"{result} is the result of comparing {nameof(c1)} and {nameof(c2)} by equality");
+
+
+            result = c1 < c2; //We can also compare the greater character
+            Console.WriteLine($"{result} is the result of comparing {nameof(c1)} and {nameof(c2)} greater");
+        }
+
+    }
+}
