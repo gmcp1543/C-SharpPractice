ebb07c09b4819c41a81733cb3e0afc1c708517bc
BuildTools <unconfigured@null.spigotmc.org>
2023-04-14 22:30:37 -0400

    Added some tests

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using ConsoleApp1;
+using ConsoleApp1.VariablesAndTypes;
 
+Variables variables = new Variables();
+variables.Test();
+
+Integers integers = new Integers();
+integers.Test();
+
+Chars chars = new Chars(); 
+chars.Test();
