cdf3abc52e3f6871ba559a71bb4aeafdde880b6a
BuildTools <unconfigured@null.spigotmc.org>
2023-04-14 22:29:43 -0400

    Changed namedspace

--- ConsoleApp1/VariablesAndTypes/Variables.cs
+++ ConsoleApp1/VariablesAndTypes/Variables.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1.Variables_and_types
+namespace ConsoleApp1.VariablesAndTypes
 {
     internal class Variables : ITestClass
     {
